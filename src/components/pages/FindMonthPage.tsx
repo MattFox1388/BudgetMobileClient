30d58096e8039f507ea4d6b719b9cceae6fb97bc
matt fox <mzorro10222@gmail.com>
2022-11-18 14:42:33 -0600

    add dropdown to findMonthPage

--- src/components/pages/FindMonthPage.tsx
+++ src/components/pages/FindMonthPage.tsx
@@ -1,15 +1,49 @@
-import React from "react";
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View, ActivityIndicator} from 'react-native';
+import {SelectList} from 'react-native-dropdown-select-list';
+import {BUDGET_API_URL} from '@env';
+import {fetchWithRetries} from '../../shared/FetchUtility';
+import {MonthStatResponse} from '../../../types/MonthStatResponse';
 
 export const FindMonthPage: React.FC = () => {
-    return (
-        <View
+  const [monthStatData, setMonthStatData] = useState<MonthStatResponse[]>([]);
+  const [selected, setSelected] = React.useState([]);
+  let selectListData: {key: number; value: string}[] = [];
+
+  const getMonthStat = () => {
+    fetchWithRetries(BUDGET_API_URL + '/get_month_stats', {}, 1).then(
+      (data: MonthStatResponse[]) => {
+        console.log(data);
+        setMonthStatData(data);
+        selectListData = data.map((row, index) => {
+          return {key: index, value: row.date_val + row.year_num};
+        });
+      },
+    );
+  };
+
+  useEffect(() => {
+    getMonthStat();
+  }, []);
+
+  return (
+    <View
       style={{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Find Month Page</Text>
+      {monthStatData ? (
+        <SelectList
+          data={selectListData}
+          save="key"
+          onSelect={() => console.log(selected)}
+          setSelected={(val: React.SetStateAction<never[]>) => setSelected(val)}
+          defaultOption={{key: null, value: null}}
+        />
+      ) : (
+        <ActivityIndicator size="large" />
+      )}
     </View>
-    );
+  );
 };
---
+++ types/MonthStatResponse.ts
@@ -0,0 +1,18 @@
+export interface MonthStatResponse  {
+    date_val: string,
+    expenses_actual: number,
+    expenses_planned: number,
+    id: number,
+    month_id: number,
+    needs_actual: number,
+    needs_planned: number,
+    other_actual: number,
+    other_planned: number,
+    paycheck_actual: number,
+    paycheck_planned: number,
+    savings_actual: number,
+    savings_planned: number,
+    wants_actual: number,
+    wants_planned: number,
+    year_num: number
+}
